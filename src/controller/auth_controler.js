f2cb6c3d09688593a5240cd2cddafac7abdf495b
yuvraj66 <srijald15@gmail.com>
2022-03-15 16:15:21 +0545

    fixes

--- src/controller/auth_controler.js
+++ src/controller/auth_controler.js
@@ -5,36 +5,35 @@ const { validateUser, User } = require("../models/User");
 
 module.exports.register = asyncHandler(async (req, res) => {
     const { error } = validateUser(req.body);
-    if (error)
+if (error)
         return res.status(401).send(messageHandler(false, error.message, 401));
     const checkUser = await User.find({
         email: req.body.email,
     });
-    if (checkUser.length > 0) {
-        return res
-            .status(401)
-            .send(messageHandler(false, "User already exists", 401));
-    }
-    const user = await User.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
-    });
-    const userData = {
-        id: user._id,
-        name: user.name,
-        email: user.email,
-        createdAt: user.createdAt,
-        updatedAt: user.updatedAt,
-    };
+    if(checkUser.length > 0){
+        return res.status(401).send(messageHandler(false, "User already exists", 401));
+   }
+        const user = await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+        });
 
-    const token = await user.generateToken(user);
-    res.cookie("Authorization", token, {
-        maxAge: 1000 * 7 * 24 * 60 * 60,
-        httpOnly: true,
-    });
+        const token = await user.generateToken(user);
+        user.accessToken = token;
+        let userData = {
+            uid: user._id,
+            name: user.name,
+            email: user.email,
+        };
 
-    return res.status(201).send(messageHandler(true, userData, 201));
+        res.cookie("Authorization", token, {
+            maxAge: 1000 * 7 * 24 * 60 * 60,
+            httpOnly: true,
+        });
+
+        return res.status(201).send(messageHandler(true, userData, 201));
+
 });
 
 module.exports.login = asyncHandler(async (req, res) => {
@@ -45,17 +44,13 @@ module.exports.login = asyncHandler(async (req, res) => {
     if (user.message)
         return res.status(401).send(messageHandler(false, user.message, 401));
     const accesstoken = await user.generateToken(user);
-    const userData = {
-        id: user._id,
+    await User.findByIdAndUpdate(user._id, accesstoken);
+    let userData = {
+        uid: user._id,
         name: user.name,
+        role: user.role,
         email: user.email,
-        createdAt: user.createdAt,
-        updatedAt: user.updatedAt,
+        token: accesstoken,
     };
-
-    res.cookie("Authorization", accesstoken, {
-        maxAge: 1000 * 7 * 24 * 60 * 60,
-        httpOnly: true,
-    });
     return res.status(201).send(messageHandler(true, userData, 201));
 });
