134cde322299896dd51c9555164b86ae74507b53
yuvraj66 <srijald15@gmail.com>
2022-03-31 09:41:51 +0545

    allowIfLogin removed

--- src/routes/todo_routes.js
+++ src/routes/todo_routes.js
@@ -1,23 +1,15 @@
 const { Router } = require("express");
 const todoController = require("../controller/todo_controller");
-const {allowIfLogin} = require("../middleware/allowIfLogin");
+const { allowIfLogin } = require("../middleware/allowIfLogin");
 const router = Router();
 
-router.post("/create/user=:userId", allowIfLogin, todoController.createTodo);
+router.post("/create/user=:userId", todoController.createTodo);
 router.delete(
   "/delete/:todoId/user=:userId",
   allowIfLogin,
   todoController.deleteTodo
 );
-router.get("/getTodos/user=:userId",  todoController.getTodos);
-router.put(
-  "/update/:todoId/user=:userId",
-  allowIfLogin,
-  todoController.updateTodo
-);
-router.get(
-  "/completed-todos/user=:userId",
-  allowIfLogin,
-  todoController.getCompletedTodos
-);
+router.get("/getTodos/user=:userId", todoController.getTodos);
+router.put("/update/:todoId/user=:userId", todoController.updateTodo);
+router.get("/completed-todos/user=:userId", todoController.getCompletedTodos);
 module.exports = router;
